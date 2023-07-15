6a482f527cf6983af6c8b85b6cad676bc2690bfc
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-07-15 02:17:48 +0200

    fix(alarmer): use pydub instead of playsound
    pydub provides a built-in support for wave files on all platforms (X OS, Linux, Windows) and should be a better fit in this case

--- src/alarmer/__main__.py
+++ src/alarmer/__main__.py
@@ -1,11 +1,12 @@
-import playsound
-
+from pydub import AudioSegment
+from pydub.playback import play
 
 class Alarmer:
-    
+
     @staticmethod
     def playsound():
-        playsound.playsound('../../assets/alarm.wav', block=True)
+        sound = AudioSegment.from_wav("../../assets/alarm.wav")
+        play(sound)
 
 
 Alarmer.playsound()
